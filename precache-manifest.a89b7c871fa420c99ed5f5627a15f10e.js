fb5c8418eb7701a663f0560dff2e15aac8e2a756
evoluteur <ogiulieri@gmail.com>
2020-06-04 16:38:23 -0700

    First build.

---
+++ precache-manifest.a89b7c871fa420c99ed5f5627a15f10e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "170ba825b9f575ffcba58fa3939eca4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b3cc41cdb42dd21482a",
+    "url": "/static/css/main.852b685a.chunk.css"
+  },
+  {
+    "revision": "1b0e578eb1790dca620f",
+    "url": "/static/js/2.feb54237.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.feb54237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b3cc41cdb42dd21482a",
+    "url": "/static/js/main.61b7c1a3.chunk.js"
+  },
+  {
+    "revision": "cc4e551538d06286f6f7",
+    "url": "/static/js/runtime-main.fd05245c.js"
+  }
+]);
